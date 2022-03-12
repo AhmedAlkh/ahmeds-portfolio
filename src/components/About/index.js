13f6382a41e650d234769dc179ce129a6b76dc55
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-03-12 10:11:54 -0500

    fixed positioning of about me image, added styling to header

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,19 +5,21 @@ function About() {
   return (
     <section className="my-5">
       <h1 id="about">About me</h1>
+      <div className="aboutMePic">
       <img
         src={personalPhoto}
         className="my-2"
         style={{ width: "25%" }}
         alt="selfie"
       />
+      </div>
       <p>
           Welcome to my portfolio! My name is Ahmed Alkhatib and the bulk of my work experience has been
           in construction and warehousing. I've always had an interest in technology and in 2021 I decided
           to enroll in a coding program offered by University of Toronto. The program I enrolled in focused
           on website and mobile app development, teaching me to become a full-stack developer! I look 
           forward to learning more and exploring new opportunities. In my personal time I enjoy working out in the gym,
-          driving my car while listening to music, playing video games, and hanging out with friends and family.
+          driving my car while listening to music, playing video games, and spending time with friends and family.
       </p>
     </section>
   );
